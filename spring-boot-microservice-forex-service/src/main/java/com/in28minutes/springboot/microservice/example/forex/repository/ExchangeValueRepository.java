860d3d7d686f94c9f5112a666d985d0f26dd6d7c
donyfutura <danilakanash999@bk.ru>
2021-03-25 00:51:56 +0300

    MicroservicesApp

---
+++ spring-boot-microservice-forex-service/src/main/java/com/in28minutes/springboot/microservice/example/forex/repository/ExchangeValueRepository.java
@@ -0,0 +1,11 @@
+package com.in28minutes.springboot.microservice.example.forex.repository;
+
+import com.in28minutes.springboot.microservice.example.forex.entity.ExchangeValue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+
+    ExchangeValue findByFromAndTo(String from, String to);
+
+
+}
